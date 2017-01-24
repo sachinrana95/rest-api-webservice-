5dd9dc072038bc58be68323b36f975c086f5f286
Sachin Rana <sachinpanwarrana30@gmail.com>
2017-01-24 22:42:49 +0530

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php
+	// $mysqli = new mysqli('localhost', 'root', 'root', 'web_service');
+	mysql_connect('localhost', 'root', 'root');
+	mysql_select_db('web_service');
+	// if(!$mysqli)
+	// {
+	// 	echo 'Connection Fail';
+	// }
+	// else
+	// {
+	// 	echo 'Connection Success';
+	// }
+	require 'functions.php';
+ ?>
